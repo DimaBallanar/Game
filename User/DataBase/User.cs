905e73af800d8e016a67d9049036c4ab138115c7
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-05 17:32:40 +0300

    update

--- User/DataBase/User.cs
+++ User/DataBase/User.cs
@@ -2,19 +2,16 @@
 namespace DataBase;
 public class User
 {
-    //public int Id { get; set; }
-    public string Name { get; set; }
+       public string Name { get; set; }
     public string Password { get; set; }
-    //public string Email { get; set; }
-
-    
+     
 
     public User(/*int id,*/ string name, string password, string email) //: this(id)
     {
-        //Id = id;
+    
         Name = name;
         Password = password;
-        //Email = email;
+    
     }
 }
 
