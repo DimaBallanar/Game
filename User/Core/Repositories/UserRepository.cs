a313a41fd29ac40f379bee483517a269c982d7ae
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-06 22:22:36 +0300

    update

---
+++ User/Core/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace User.Core.Repositories
+{
+    internal class UserRepository
+    {
+        public List<User>GetAll()
+        {
+            JsonArray
+        }
+    }
+}
