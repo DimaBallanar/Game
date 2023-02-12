47f147b2dc78487aef1bd75867ca7701aaf36125
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-13 00:52:12 +0300

    update

---
+++ BlackJack/GameJack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    public class GameJack
+    {
+        private Deck deck = new();
+        private Player[] players;
+
+        public Game()
+        {
+            players = new[]
+            {
+             new Player("Дилер", deck),
+             new Player("Игрок", deck)
+         };
+
+            Start();
+        }
+
+    }
+}
