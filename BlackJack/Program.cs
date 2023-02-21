3b77cc4d9f9e71e642a53c416faa3b19738474bd
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-21 20:23:46 +0300

    update

--- BlackJack/Program.cs
+++ BlackJack/Program.cs
@@ -3,83 +3,12 @@ using System;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
-    public class Game
+   internal class Game
     {
-        private CardDeck deck = new();
-        private Player[] players;
-        public int NumPlayers { get; set; }
-
-        public Game(int numPlayers)
+        static void Main(string[] args)
         {
-
-            players = new Player[numPlayers];
-            for (int i = 0; i < players.Length; i++)
-            {
-                players[i] = new Player($"игрок{i}", deck);
-            }
-            Start();
+            GameClient game = new GameClient(2);
         }
 
-        public bool IsEnded { get; private set; }
-
-        public void Start()
-        {
-            bool replay = true;
-            while (replay)
-            {
-                Console.Clear();
-                Turn();
-                IsEnded = players.Any(x => x.IsLost);
-
-                Console.ReadLine();
-
-                if (IsEnded)
-                    replay = Replay();
-            }
-        }
-
-        private void Turn()
-        {
-            foreach (var player in players)
-            {
-                var status = player.Take() ? "Еще в игре" : "Проиграл";
-                Console.WriteLine($"У {player.Name} {player.Score}, он {status}");
-            }
-        }
-
-        private bool Replay()
-        {
-            bool result = false;
-
-            while (true)
-            {
-                Console.Clear();
-                Console.Write("Повторить? (y/n): ");
-                var value = Console.ReadLine();
-
-                if (value is "y" or "n")
-                {
-                    result = value is "y";
-                    break;
-                }
-            }
-
-            if (result)
-            {
-                Reset();
-            }
-
-            return result;
-        }
-
-        private void Reset()
-        {
-            deck = new();
-            foreach (var player in players)
-            {
-                player.Reset();
-            }
-        }
     }
-
 }
