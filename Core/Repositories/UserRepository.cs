f7effe093fe97f2c2d2dd792cc65ff9f07d1ea9f
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-22 23:48:06 +0300

    up

--- Core/Repositories/UserRepository.cs
+++ Core/Repositories/UserRepository.cs
@@ -10,7 +10,7 @@ namespace Core.Repositories
 {
     public class UserRepository : BaseRepository<User>
     {
-        protected override string path { get; } = AppDomain.CurrentDomain.BaseDirectory;
+        protected override string Path  => AppDomain.CurrentDomain.BaseDirectory+"users.txt";
         public User GetByName(string login)
         {
             List<User> userList = GetAll();
