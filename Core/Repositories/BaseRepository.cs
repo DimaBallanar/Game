4b05f28bf1fbee17ebab0feb9f6c8048f2c9edc4
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-27 12:27:28 +0300

    fix trouble

--- Core/Repositories/BaseRepository.cs
+++ Core/Repositories/BaseRepository.cs
@@ -11,32 +11,38 @@ namespace Core.Repositories
     public abstract class BaseRepository<T>
     {
         protected abstract string Path { get; }
-        public List<T> GetAll()
+        protected IEnumerable<T> GetAll()
         {
-            List<T> data = new List<T>();
+            //List<T> data = new List<T>();
             var serializeoptions = new JsonSerializerOptions
             {
                 PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
             };
-            try
-            {
+            //try
+            //{
                 using StreamReader sr1 = new StreamReader(Path);
+                //for (string line = sr1.ReadLine(); line != null; line = sr1.ReadLine())
+                //{
+                //    data.Add(JsonSerializer.Deserialize<T>(line, serializeoptions));
+                //}
                 for (string line = sr1.ReadLine(); line != null; line = sr1.ReadLine())
                 {
-                    data.Add(JsonSerializer.Deserialize<T>(line, serializeoptions));
+                    //data.Add(JsonSerializer.Deserialize<T>(line, serializeoptions));
+                    yield return JsonSerializer.Deserialize<T>(line, serializeoptions);
+
                 }
-                
+
                 sr1.Close();
-                return data;
-            }
-            catch (FileNotFoundException)
-            {
-                return data;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+                //return data;
+            //}
+            //catch (FileNotFoundException)
+            //{
+            //    return data;
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw ex;
+            //}
         }
         public void UpdateFile(List<T> userList)
         {
