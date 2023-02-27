09b12b6d75e715543b23e3f4cdb8601f06884005
Dima Ballanar <D.Ballanar@gmail.com>
2023-02-27 12:27:40 +0300

    fix trouble

--- Core/Repositories/StatsRepository.cs
+++ Core/Repositories/StatsRepository.cs
@@ -15,17 +15,17 @@ namespace Core.Repositories
         public void AddUserStats(UserStats userStats)
         {
             if (userStats == null) throw new ArgumentNullException(nameof(userStats));
-            List<UserStats> users = GetAll();
+            List<UserStats> users = GetAll().ToList();
             users.Add(userStats);
             UpdateFile(users);
         }
         public List<UserStats> GetUserStats(int id)
         {
-            List<UserStats> users = GetAll();
+            List<UserStats> users = GetAll().ToList();
             List<UserStats> stats = new List<UserStats>();
             for (int i = 0; i < users.Count; i++)
             {
-                if (users[i].User.Id == id)
+                if (users[i].Id == id)
                 {
                     stats.Add(users[i]);
                 }
@@ -34,7 +34,7 @@ namespace Core.Repositories
         }
         public List<UserStats> GetAllStats()
         {
-            List<UserStats> users = GetAll();
+            List<UserStats> users = GetAll().ToList();
             return users;
         }
     }
