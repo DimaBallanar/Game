3c93bef13e99ec4c59174611b04e5dff37a98a8a
Dima Ballanar <D.Ballanar@gmail.com>
2023-03-28 18:06:28 +0300

    up

--- BlackJack/LoadGame.cs
+++ BlackJack/LoadGame.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,78 @@ namespace BlackJack
     public class LoadGame
     {
         Deck deck;
-        
+        public string NameGame { get; set; }
+        public LoadGame()
+        {
+            NameGame = "BlackJack";
+            //  deck = new Deck();
+            //Hand player= new Hand();
+        }
+        public void Start(out string result)
+        {
+            result = null;   
+            //Player player = new Player();
+            Hand playerHand = player.Deal(deck);
+
+            if (playerHand.IsBusted())
+            {
+                Console.WriteLine("Game Over!");
+                result = "loss";
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Player stays at {0}", playerHand.Total());
+                Console.ReadKey();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Играет Диллер");
+            Console.ReadKey();
+
+            Dealer dealer = new Dealer();
+            Hand dealerHand = dealer.Deal(deck);
+
+            Console.WriteLine();
+            if (dealerHand.IsBusted())
+            {
+                Console.Write("Диллер выиграл!");
+                Console.WriteLine("Игрок проиграл!");
+                result = "loss";
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine();
+            if (playerHand.Total() > dealerHand.Total())
+            {
+                Console.WriteLine("игрок {0} победил дилера {1}", playerHand.Total(), dealerHand.Total()); ;
+                Console.WriteLine("Player Wins!");
+                result = "WIN";
+                Console.ReadKey();
+                return;
+            }
+            else if (playerHand.Total() < dealerHand.Total())
+            {
+                Console.WriteLine("дилер {1} победил игрока {0}", playerHand.Total(), dealerHand.Total());
+                Console.WriteLine("Игрок проиграл!");
+                result = "Loss";
+                Console.ReadKey();
+                return;
+            }
+            else
+            {
+                Console.WriteLine("дилер {1} = игрок {0}", playerHand.Total(), dealerHand.Total());
+                Console.WriteLine("Ничья");
+                result = "Ничья";
+                Console.ReadKey();
+                return;
+            }
+        }
+        static void myHandler(object sender, ConsoleCancelEventArgs args)
+        {
+
+        }
     }
 }
